d3805ecb6f10e411039d18d47641eddf0ea9c950
Lorenzo Manicardi <lorenzomanicardi@192.168.1.93>
2022-03-03 11:37:30 +0100

    added migrations for all user, post and comment models

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,53 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->get('/home', function () use ($router) {
-    return "HOME";
+/*
+ *
+ * UNAUTHENTICATED ROUTES
+ *
+ */
+
+//JWT
+$router->group(['prefix' => '/api/v1'], function( $router ) {
+        $router->post( '/authenticate', 'AuthController@login');
+        $router->post( '/register', 'AuthController@register' );
+    }
+);
+
+//POSTS
+$router->group(['prefix' => '/api/v1/posts'], function( $router ) {
+    $router->get( '/', 'PostController@index' );
+    $router->get( '/{id}', 'PostController@show' );
 });
+
+/*
+ *
+ * AUTHENTICATED ROUTES
+ *
+ */
+
+//JWT
+$router->group(['prefix' => '/api/v1', 'middleware' => 'auth'], function( $router ) {
+        $router->post( '/logout', 'AuthController@logout' );
+        $router->get( '/refresh', 'AuthController@refresh' );
+        $router->post( '/refresh', 'AuthController@refresh' );
+});
+
+//POSTS
+$router->group(['prefix' => '/api/v1/posts', 'middleware' => 'auth'], function( $router ) {
+    $router->post( '/', 'PostController@store' );
+    $router->put( '/{id}', 'PostController@update' );
+    $router->delete( '/{id}', 'PostController@destroy' );
+});
+
+//COMMENTS
+$router->group(['prefix' => '/api/v1/posts', 'middleware' => 'auth'], function( $router ) {
+    $router->post( '/{post_id}/comments', 'CommentController@store' );
+    $router->put( '/{post_id}/comments/{id}', 'CommentController@update' );
+    $router->delete( '{post_id}/comments/{id}', 'CommentController@destroy' );
+});
+
