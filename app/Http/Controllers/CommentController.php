0cfa992cf14469f9ee5fa1870b8a3502bf625eb2
Lorenzo Manicardi <lorenzomanicardi@fattureincloud.it>
2022-03-07 11:34:39 +0100

    improved use of new middleware removing useless string returns from controllers

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -14,60 +14,49 @@ class CommentController extends Controller
     /**
      * @param Request $request
      * @param string $post_id
-     * @return array|string[]
+     * @return Comment
      * @throws ValidationException
      */
-    public function store(Request $request, string $post_id): array
+    public function store(Request $request, string $post_id): Comment
     {
         $this->validate($request, [
             "content" => 'string|required'
         ]);
         /** @var Post $post */
         $post = Post::findOrFail($post_id);
-        if ($post) {
-            try {
-                $newComment = new Comment();
-                $newComment->user_id = Auth::user()->id;
-                $newComment->post_id = $post->id;
-                $newComment->content = $request->input('content');
-                $newComment->save();
-                return ["status" => "success", "message" => "comment created successfully!"];
-            } catch(Throwable $e) {
-                return ["status" => "error", "message" => $e];
-            }
-        } else {
-            return ["status" => "error", "message" => "post not found!"];
-        }
+        $newComment = new Comment();
+        $newComment->user_id = Auth::user()->id;
+        $newComment->post_id = $post->id;
+        $newComment->content = $request->input('content');
+        $newComment->save();
+        return $newComment;
     }
 
     /**
      * @param Request $request
      * @param string $post_id
      * @param string $id
-     * @return array|string[]
+     * @return string[]|Comment
      * @throws ValidationException
      */
-    public function update(Request $request, string $post_id, string $id): array
+    public function update(Request $request, string $post_id, string $id)
     {
         if (Auth::user()->subscription == 'premium') {
             $this->validate($request, [
                 "content" => 'string|required'
             ]);
+            /** @var Comment $comment */
             $comment = Auth::user()->comments()->where('post_id', $post_id)->findOrFail($id);
-            try {
-                $comment->content = $request->input('content');
-                $comment->save();
-                return ["status" => "success", "message" => "comment updated successfully!"];
-            } catch(Throwable $e) {
-                return ["status" => "error", "message" => $e];
-            }
+            $comment->content = $request->input('content');
+            $comment->save();
+            return $comment;
         } else {
             return ["status" => "error", "message" => "you must be a premium user for this feature!"];
         }
-
     }
 
     /**
+     * domanda per Steve: qua meglio che la funzione ritorni qualche altro tipo di dato o ha senso così?
      * @param string $post_id
      * @param string $id
      * @return array|string[]
@@ -76,6 +65,7 @@ class CommentController extends Controller
     {
         if (Auth::user()->subscription == 'premium') {
             try {
+                /** @var Comment $comment */
                 $comment = Auth::user()->comments()->where('post_id', $post_id)->findOrFail($id);
                 $comment->delete();
                 return ["status" => "success", "message" => "comment deleted successfully!"];
