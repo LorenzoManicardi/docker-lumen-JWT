2e38aab32882899324519ef52b3112421f8040a6
Lorenzo Manicardi <lorenzomanicardi@fattureincloud.it>
2022-03-08 16:40:50 +0100

    fix

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use phpDocumentor\Reflection\DocBlock\Tags\Reference\Fqsen;
 
 /**
  * @property string title
